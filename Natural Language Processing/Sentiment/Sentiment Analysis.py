3a490647dc0f45a67939aae9641eec67cd38a922
denes-panta <denes-panta@users.noreply.github.com>
2017-11-25 00:59:52 +0100

    Sentiment Analyser
    Basic Sentiment Analyser

--- Natural Language Processing/Sentiment/Sentiment Analysis.py
+++ Natural Language Processing/Sentiment/Sentiment Analysis.py
@@ -5,90 +5,128 @@ import nltk
 import random
 import re
 
-#Functions
-def list_txt(path, split = 0.95, pn = False):
-    #split = percentage of reviews as training data
-    #path = path of the files
-    #pn = positive = True, negative = False
-    
-    reviews = []
-    tokenizer = nltk.tokenize.RegexpTokenizer(r'((?<=[^\w\s])\w(?=[^\w\s])|(\W))+', gaps = True) #Define the tokenizer
-    
-    for i, filename in enumerate(os.listdir(path)): #Import reviews and tokenize
-        reviews.insert(i, [list(tokenizer.tokenize(open(path + filename).read())), pn])
-    
-    p = int(len(reviews) * split) #define the splitting point
-    
-    train = reviews[0:p] #Train data
-    test = reviews[p:len(reviews)]  #Test data 
-    
-    return train, test #Return the matrices
+class sentiment(object):
 
-
-def words(texts): #preprocessing
-    stopWords = set(nltk.corpus.stopwords.words('english') + list((' ', '\n'))) #define the stopwords
-    ttab = str.maketrans(string.punctuation, ' ' * len(string.punctuation)) #translate punctuations to ' '
+    def __init__(self, p, sp, n):
+        #Import reviews and create train & test data
+        self.neg_train, self.neg_test = self.list_txt(p, split = sp, pn = 'neg')
+        self.pos_train, self.pos_test = self.list_txt(n, split = sp, pn = 'pos')
     
-    for text in texts:
-        text[0] = [word.lower().translate(ttab) for word in text[0]] #lower case the words and translate punctuations
-        text[0] = [re.sub(r'\d+', ' ', word) for word in text[0] if word] #filter out digits
-        text[0] = [word for word in text[0] if word not in stopWords] #filter out the stopwords
-        text[0] = [nltk.stem.porter.PorterStemmer().stem(word) for word in text[0]] #Stem the remaining words
+        self.l_test = self.pos_test + self.neg_test
+        random.shuffle(self.l_test)
 
-    return texts
-
-def word_freq(texts): #calculate the word frequencies
-    all_words = []
-    
-    for text in texts:
-        for words in text[0]:
-            all_words.append(words)
-    
-    all_words = nltk.FreqDist(all_words)
-    
-    return all_words
-
-def word_feat(text, probdist, top = 1000): #turn the words into features
-    word_features = []
-    features = {}
-
-    for word in probdist.most_common()[:top]: #take the most common words
-        word_features.append(word[0])
+        self.l_train = self.pos_train + self.neg_train
+        random.shuffle(self.l_train)
         
-    for word in word_features:
-        features[word] = (word in text[0])
-            
-    return features #return the features
+        #Preprocessing    
+        self.l_test = self.words(self.l_test)
+        self.l_train = self.words(self.l_train)
+        
+        #Calculate the word frequencies
+        self.t_wfreq = self.word_freq((self.l_train + self.l_test))
+        
+        #Turn the words into features & Take the top 'top' features
+        self.feats_train = self.feat_create(self.l_train, top = 1000)
+        self.feats_test = self.feat_create(self.l_test, top = 1000)
 
-def feat_create(train, top = 1000): #take the top 'top' features
-    feats = []
+    def list_txt(self, path, split = 0.95, pn = False):
+        #split = percentage of reviews as training data
+        #path = path of the files
+        #pn = positive = True, negative = False
+        
+        reviews = []
+        
+        #Define the tokenizer
+        tokenizer = nltk.tokenize.RegexpTokenizer(
+                r'((?<=[^\w\s])\w(?=[^\w\s])|(\W))+', gaps = True) 
+        
+        #Import reviews and tokenize
+        for i, filename in enumerate(os.listdir(path)): 
+            reviews.insert(
+                    i, 
+                    [list(tokenizer.tokenize(open(path + filename).read())), pn]
+                    )
+        
+        #Define the splitting point
+        p = int(len(reviews) * split) 
+        
+        train = reviews[0:p]
+        test = reviews[p:len(reviews)]
+        
+        return train, test
+
+    #Preprocessing
+    def words(self, texts): 
+        
+        #Define Stopwords
+        stopWords = set(nltk.corpus.stopwords.words('english') + \
+                        list((' ', '\n')))
+        
+        #Translate punctuations to ' '
+        ttab = str.maketrans(string.punctuation, ' ' * len(string.punctuation)) 
+        
+        for text in texts:
+            #lower case the words and translate punctuations
+            text[0] = [w.lower().translate(ttab) for w in text[0]]
+            #filter out digits
+            text[0] = [re.sub(r'\d+', ' ', w) for w in text[0] if w] 
+            #filter out the stopwords
+            text[0] = [w for w in text[0] if w not in stopWords] 
+            #Stem the remaining words
+            text[0] = [nltk.stem.porter.PorterStemmer().stem(w) for w in text[0]] 
     
-    for text in train:
-        feats.append((word_feat(text[0], t_wfreq, top), text[1])) 
+        return texts
     
-    return feats
+    #Calculate the word frequencies
+    def word_freq(self, texts): 
+        all_words = []
+        
+        for text in texts:
+            for words in text[0]:
+                all_words.append(words)
+        
+        all_words = nltk.FreqDist(all_words)
+        
+        return all_words
+    
+    #Turn the words into features
+    def word_feat(self, text, probdist, top = 1000): 
+        word_features = []
+        features = {}
+        
+        #Take the most common words
+        for word in probdist.most_common()[:top]: 
+            word_features.append(word[0])
+            
+        for word in word_features:
+            features[word] = (word in text[0])
+                
+        return features
+    
+    #Take the top 'top' features
+    def feat_create(self, train, top = 1000): 
+        feats = []
+        
+        for text in train:
+            feats.append((self.word_feat(text[0], self.t_wfreq, top), text[1])) 
+        
+        return feats
+
+    #Classifier
+    def classify(self):
+        #Define the classifier
+        classifier = nltk.NaiveBayesClassifier.train(self.feats_train) 
+        #Print the test accuracy
+        print("Testing Accuracy (NBC): ", (nltk.classify.accuracy(classifier, self.feats_test)) * 100) 
+        
+        #return the most informative features
+        return classifier.most_informative_features(25)
 
 #Script
 #Pre-processing
-neg_train, neg_test = list_txt("F:/Code/Sentiment/neg/", pn = 'neg')
-pos_train, pos_test = list_txt("F:/Code/Sentiment/pos/", pn = 'pos')
-
-l_test = pos_test + neg_test
-random.shuffle(l_test)
-
-l_train = pos_train + neg_train
-random.shuffle(l_train)
-
-del neg_test, pos_test, neg_train, pos_train
-
-l_test = words(l_test)
-l_train = words(l_train)
-
-t_wfreq = word_freq((l_train + l_test))
-feats_train = feat_create(l_train, top = 1000)
-feats_test = feat_create(l_test, top = 1000)
-
-#Classifier
-classifier = nltk.NaiveBayesClassifier.train(feats_train) # define the classifier
-print("Testing Accuracy (NBC): ", (nltk.classify.accuracy(classifier, feats_test)) * 100) #print the test accuracy
-classifier.most_informative_features(25) #return the most inforamtive features
+if __name__ == "__main__":
+    analysis = sentiment(p = "F:/Code/Sentiment/neg/",
+                         n = "F:/Code/Sentiment/pos/",
+                         sp = 0.95)
+    most_informative = analysis.classify()
+    print(most_informative)
