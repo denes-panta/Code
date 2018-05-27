24714f9f172750aa94f76dd4c98f6cf7fe43b603
denes-panta <denes-panta@users.noreply.github.com>
2018-05-27 18:22:42 +0200

    MNIST

--- Samples/Deep Learning - PyTorch/Image Classification/MNIST - PyTorch - ANN.py
+++ Samples/Deep Learning - PyTorch/Image Classification/MNIST - PyTorch - ANN.py
@@ -125,19 +125,45 @@ testLoader = torch.utils.data.DataLoader(
     )
 
 
-#Validate
-NeuralNet(trainX = tValTrainX, 
-          trainY = tValTrainY, 
-          testX = tValTestX, 
-          testY = tValTestY,
-          e = 4000, 
-          mode = "validate"
-          )
+if use_cuda:
+    model = model.cuda()
 
-#Train
-tTestY = NeuralNet(trainX = tTrainX, 
-                   trainY = tTrainY, 
-                   testX = tTestX, 
-                   e = 4000, 
-                   mode = "train"
-                   )
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+criterion = nn.CrossEntropyLoss()
+
+for epoch in range(10):
+    # trainning
+    ave_loss = 0
+    for batch_idx, (x, target) in enumerate(train_loader):
+        if use_cuda:
+            x, target = x.cuda(), target.cuda()
+        x, target = Variable(x), Variable(target)
+        optimizer.zero_grad()
+
+        out = model(x)
+        loss = criterion(out, target)
+        ave_loss = ave_loss * 0.9 + loss.data[0] * 0.1
+        loss.backward()
+        optimizer.step()
+        if (batch_idx+1) % 100 == 0 or (batch_idx+1) == len(train_loader):
+            print '==>>> epoch: {}, batch index: {}, train loss: {:.6f}'.format(
+                epoch, batch_idx+1, ave_loss)
+    # testing
+    correct_cnt, ave_loss = 0, 0
+    total_cnt = 0
+    for batch_idx, (x, target) in enumerate(test_loader):
+        if use_cuda:
+            x, target = x.cuda(), target.cuda()
+        x, target = Variable(x, volatile=True), Variable(target, volatile=True)
+        out = model(x)
+        loss = criterion(out, target)
+        _, pred_label = torch.max(out.data, 1)
+        total_cnt += x.data.size()[0]
+        correct_cnt += (pred_label == target.data).sum()
+        # smooth average
+        ave_loss = ave_loss * 0.9 + loss.data[0] * 0.1
+        
+        if(batch_idx+1) % 100 == 0 or (batch_idx+1) == len(test_loader):
+            print '==>>> epoch: {}, batch index: {}, test loss: {:.6f}, acc: {:.3f}'.format(
+epoch, batch_idx+1, ave_loss, correct_cnt * 1.0 / total_cnt)
